4d564e839845b266535380acb02be5df4471e70f
Lucas Souza <lss8@cin.ufpe.br>
2023-03-01 20:17:35 -0300

    correção da tabela de criação, e adição do metodo CHECK

--- scripts/creation.sql
+++ scripts/creation.sql
@@ -1,87 +1,87 @@
 CREATE TABLE Pessoa ( 
-  CPF varchar(14), 
-  Data_Nasc date NOT NULL, 
-  Nome varchar(255) NOT NULL, 
-  Sobrenome varchar(255) NOT NULL, 
+  CPF VARCHAR2(14), 
+  Data_Nasc DATE NOT NULL, 
+  Nome VARCHAR2(255) NOT NULL, 
+  Sobrenome VARCHAR2(255) NOT NULL, 
   CONSTRAINT Pessoa_pkey PRIMARY KEY (CPF) 
 );
 
 CREATE TABLE Endereco (   
-  CEP varchar(9),   
-  Numero number(4),   
-  Cidade varchar(255) NOT NULL,   
-  Bairro varchar(255) NOT NULL,  
-  Rua varchar(255) NOT NULL,  
+  CEP VARCHAR2(9),   
+  Numero NUMBER(4),   
+  Cidade VARCHAR2(255) NOT NULL,   
+  Bairro VARCHAR2(255) NOT NULL,  
+  Rua VARCHAR2(255) NOT NULL,  
   CONSTRAINT Endereco_pkey PRIMARY KEY (Numero, CEP)  
 );
 
 CREATE TABLE Clube (     
-  CNPJ varchar(18),     
-  Nome_clube varchar(255) NOT NULL,     
-  Email varchar(255) NOT NULL,      
-  CEP varchar(9) NOT NULL,     
-  Number_endereco number(4) NOT NULL,    
+  CNPJ VARCHAR2(18),     
+  Nome_clube VARCHAR2(255) NOT NULL,     
+  Email VARCHAR2(255) NOT NULL,      
+  CEP VARCHAR2(9) NOT NULL,     
+  Num_endereco NUMBER(4) NOT NULL,    
   CONSTRAINT Clube_pkey PRIMARY KEY (CNPJ),    
-  CONSTRAINT Clube_fkey1 FOREIGN KEY (Number_endereco, CEP) REFERENCES Endereco(Numero, CEP) 
+  CONSTRAINT Clube_fkey1 FOREIGN KEY (Num_endereco, CEP) REFERENCES Endereco(Numero, CEP) 
 );
 
-CREATE TABLE Time (    
-  Divisao varchar(5),    
-  Modalidade varchar(255) NOT NULL,   
-  Liga varchar(255) NOT NULL,   
-  CNPJ_Clube varchar(18) NOT NULL,   
-  CONSTRAINT Time_pkey PRIMARY KEY (Divisao),   
-  CONSTRAINT Time_fkey FOREIGN KEY (CNPJ_Clube) REFERENCES Clube(CNPJ)   
+CREATE TABLE Equipe (    
+  Divisao VARCHAR2(5),    
+  Modalidade VARCHAR2(255) NOT NULL,   
+  Liga VARCHAR2(255) NOT NULL,   
+  CNPJ_Clube VARCHAR2(18) NOT NULL,   
+  CONSTRAINT Equipe_pkey PRIMARY KEY (Divisao),   
+  CONSTRAINT Equipe_fkey FOREIGN KEY (CNPJ_Clube) REFERENCES Clube(CNPJ)   
 );
 
 CREATE TABLE Estadio (
-  Nome varchar(255),
-  Numero number(4) NOT NULL,
-  CEP varchar(9) NOT NULL,
-  CNPJ_Clube varchar(18),
-  Aluguel decimal(8,2) NOT NULL,
-  Lotacao number(6) NOT NULL,
+  Nome VARCHAR2(255),
+  Numero NUMBER(4) NOT NULL,
+  CEP VARCHAR2(9) NOT NULL,
+  CNPJ_Clube VARCHAR2(18),
+  Aluguel DECIMAL(8,2) NOT NULL,
+  Lotacao NUMBER(6) NOT NULL,
   CONSTRAINT Estadio_pkey PRIMARY KEY (Nome),
   CONSTRAINT Estadio_fkey1 FOREIGN KEY (Numero, CEP) REFERENCES Endereco (Numero, CEP),
   CONSTRAINT Estadio_fkey2 FOREIGN KEY (CNPJ_Clube) REFERENCES Clube (CNPJ)
 );
 
 CREATE TABLE Partida (
-  Data_Part date,
-  Adversario varchar(255) NOT NULL,
-  Vencedor varchar(255) NOT NULL,
-  CONSTRAINT Partida_pkey PRIMARY KEY (Data_Part),
+  Data_Partida DATE,
+  Adversario VARCHAR2(255) NOT NULL,
+  Vencedor VARCHAR2(255) NOT NULL,
+  CONSTRAINT Partida_pkey PRIMARY KEY (Data_Partida),
 );
 
 CREATE TABLE Cargos (
-  Funcao varchar(255),
-  Salario decimal(10,2) NOT NULL,
-  CONSTRAINT Cargos_pkey PRIMARY KEY (FUNCAO)
+  Funcao VARCHAR2(255),
+  Salario DECIMAL(10,2) NOT NULL CHECK (Salario >= 1320,00),
+  CONSTRAINT Cargos_pkey PRIMARY KEY (Funcao)
 )
   
 CREATE TABLE Telefone (
-  Pessoa varchar(14),
-  Num_Telefone number(11),
+  Pessoa VARCHAR2(14),
+  Num_Telefone NUMBER(11),
   CONSTRAINT Telefone_pkey PRIMARY KEY (Pessoa, Num_Telefone),
   CONSTRAINT Telefone_fkey FOREIGN KEY (Pessoa) REFERENCES Pessoa (CPF)
 )
   
 CREATE TABLE Jogador (
-  CPF varchar(14),
-  Numero number(2) NOT NULL,
-  Posicao varchar(255) NOT NULL,
-  Time varchar(5) NOT NULL,
-  Escalacao varchar(3) NOT NULL,
+  CPF VARCHAR2(14),
+  Numero NUMBER(2) NOT NULL,
+  Posicao VARCHAR2(255) NOT NULL,
+  Equipe_Jogador VARCHAR2(5) NOT NULL,
+  Escalacao VARCHAR2(7) NOT NULL,
   CONSTRAINT Jogador_pkey PRIMARY KEY (CPF),
   CONSTRAINT Jogador_fkey FOREIGN KEY (CPF) REFERENCES Pessoa(CPF), 
-  CONSTRAINT Jogador_fkey1 FOREIGN KEY (Time) REFERENCES Time(Divisao)
+  CONSTRAINT Jogador_fkey1 FOREIGN KEY (Equipe_Jogador) REFERENCES Equipe(Divisao)
 )
 
 CREATE TABLE Funcionario (
-  CPF varchar(14),
-  Supervisor varchar(14),
-  CNPJ varchar(255) NOT NULL,
-  Cargo varchar(5) NOT NULL,
+  CPF VARCHAR2(14),
+  Supervisor VARCHAR2(14),
+  CNPJ VARCHAR2(255) NOT NULL,
+  Cargo VARCHAR2(5) NOT NULL,
   CONSTRAINT Funcionario_pkey PRIMARY KEY (CPF),
   CONSTRAINT Funcionario_fkey FOREIGN KEY (CPF) REFERENCES Pessoa(CPF), 
   CONSTRAINT Funcionario_fkey1 FOREIGN KEY (Supervisor) REFERENCES Funcionario(CPF), 
@@ -90,33 +90,33 @@ CREATE TABLE Funcionario (
 )
 
 CREATE TABLE Cliente (
-  CPF varchar(14),
+  CPF VARCHAR2(14),
   CONSTRAINT Cliente_pkey PRIMARY KEY (CPF),
   CONSTRAINT Cliente_fkey FOREIGN KEY (CPF) REFERENCES Pessoa(CPF)
 )
 
 CREATE TABLE Loja (
-  ID varchar(2), -- defini com 2 numeros pq clube nao tem mta loja
-  CEP varchar(9) NOT NULL,
-  Numero number(4) NOT NULL,
+  ID NUMBER, -- mudei para number para usarmos o create sequence
+  CEP VARCHAR2(9) NOT NULL,
+  Numero NUMBER(4) NOT NULL,
   CONSTRAINT Loja_pkey PRIMARY KEY (ID),
   CONSTRAINT Loja_fkey FOREIGN KEY (Numero, CEP) REFERENCES Endereco (Numero, CEP)
 )
 
 CREATE TABLE Cupom (
-  Codigo varchar(6), -- defini que vai ter 6 codigos como cupons da shopee, ifood etc
-  Desconto_porcentagem varchar(3) NOT NULL,
+  Codigo VARCHAR2(6), -- defini que vai ter 6 codigos como cupons da shopee, ifood etc
+  Desconto_porcentagem NUMBER NOT NULL,
   CONSTRAINT Cupom_pkey PRIMARY KEY (Codigo)
 )
 
 CREATE TABLE Cliente_loja (
-  Data_venda varchar(8), -- modelo dd/mm/yyyy
-  Produto varchar(255),
-  ID varchar(2),
-  CPF varchar(14),
-  Codigo varchar(6),
-  Forma_pagamento varchar(255) NOT NULL,
-  Preco number(3) NOT NULL,
+  Data_venda DATE NOT NULL, -- modelo dd/mm/yyyy
+  Produto VARCHAR2(255),
+  ID NUMBER,
+  CPF VARCHAR2(14),
+  Codigo VARCHAR2(6),
+  Forma_pagamento VARCHAR2(255) NOT NULL,
+  Preco NUMBER NOT NULL,
   CONSTRAINT Cliente_loja_primkey PRIMARY KEY (Data_venda, Produto, ID, CPF),
   CONSTRAINT Cliente_loja_fkey FOREIGN KEY (ID) REFERENCES Loja(ID),
   CONSTRAINT Cliente_loja_fkey1 FOREIGN KEY (CPF) REFERENCES Cliente(CPF),
@@ -125,11 +125,11 @@ CREATE TABLE Cliente_loja (
 
 -- precisa ver isso daqui em relação a tabela de partida pq tem o problema
 CREATE TABLE Jogar (
-  Time varchar(5),
-  Nome_Estadio varchar(255),
-  Data_Partida date,
-  CONSTRAINT Jogar_pkey PRIMARY KEY (Time, Nome_Estadio, Data_Partida),
-  CONSTRAINT Jogar_fkey1 FOREIGN KEY (Time) REFERENCES Time (Nome),
+  Equipe_Jogo VARCHAR2(5),
+  Nome_Estadio VARCHAR2(255),
+  Data_Jogo DATE,
+  CONSTRAINT Jogar_pkey PRIMARY KEY (Equipe_Jogo, Nome_Estadio, Data_Partida),
+  CONSTRAINT Jogar_fkey1 FOREIGN KEY (Equipe_Jogo) REFERENCES Equipe(Divisao),
   CONSTRAINT Jogar_fkey2 FOREIGN KEY (Nome_Estadio) REFERENCES Estadio(Nome),
-  CONSTRAINT Jogador_fkey3 FOREIGN KEY (Data_Partida) REFERENCES Partida(Data_Part)
+  CONSTRAINT Jogador_fkey3 FOREIGN KEY (Data_Jogo) REFERENCES Partida(Data_Partida)
 )
