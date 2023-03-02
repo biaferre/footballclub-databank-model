97072ceb29ea46c444fcd2f33fcf787ec29274b7
claraabk <claraabk1@gmail.com>
2023-03-01 21:26:55 -0300

    fix

--- scripts/creation.sql
+++ scripts/creation.sql
@@ -50,21 +50,21 @@ CREATE TABLE Partida (
   Data_Partida DATE,
   Adversario VARCHAR2(255) NOT NULL,
   Vencedor VARCHAR2(255) NOT NULL,
-  CONSTRAINT Partida_pkey PRIMARY KEY (Data_Partida),
+  CONSTRAINT Partida_pkey PRIMARY KEY (Data_Partida)
 );
 
 CREATE TABLE Cargos (
   Funcao VARCHAR2(255),
-  Salario DECIMAL(10,2) NOT NULL CHECK (Salario >= 1320,00),
+  Salario DECIMAL(10,2) NOT NULL CHECK (Salario >= 1320.00),
   CONSTRAINT Cargos_pkey PRIMARY KEY (Funcao)
-)
+);
   
 CREATE TABLE Telefone (
   Pessoa VARCHAR2(14),
   Num_Telefone NUMBER(11),
   CONSTRAINT Telefone_pkey PRIMARY KEY (Pessoa, Num_Telefone),
   CONSTRAINT Telefone_fkey FOREIGN KEY (Pessoa) REFERENCES Pessoa (CPF)
-)
+);
   
 CREATE TABLE Jogador (
   CPF VARCHAR2(14),
@@ -75,7 +75,7 @@ CREATE TABLE Jogador (
   CONSTRAINT Jogador_pkey PRIMARY KEY (CPF),
   CONSTRAINT Jogador_fkey FOREIGN KEY (CPF) REFERENCES Pessoa(CPF), 
   CONSTRAINT Jogador_fkey1 FOREIGN KEY (Equipe_Jogador) REFERENCES Equipe(Divisao)
-)
+);
 
 CREATE TABLE Funcionario (
   CPF VARCHAR2(14),
@@ -87,13 +87,13 @@ CREATE TABLE Funcionario (
   CONSTRAINT Funcionario_fkey1 FOREIGN KEY (Supervisor) REFERENCES Funcionario(CPF), 
   CONSTRAINT Funcionario_fkey2 FOREIGN KEY (CNPJ) REFERENCES Clube(CNPJ),
   CONSTRAINT Funcionario_fkey3 FOREIGN KEY (Cargo) REFERENCES Cargos(Funcao)
-)
+);
 
 CREATE TABLE Cliente (
   CPF VARCHAR2(14),
   CONSTRAINT Cliente_pkey PRIMARY KEY (CPF),
   CONSTRAINT Cliente_fkey FOREIGN KEY (CPF) REFERENCES Pessoa(CPF)
-)
+);
 
 CREATE TABLE Loja (
   ID NUMBER, -- mudei para number para usarmos o create sequence
@@ -101,13 +101,13 @@ CREATE TABLE Loja (
   Numero NUMBER(4) NOT NULL,
   CONSTRAINT Loja_pkey PRIMARY KEY (ID),
   CONSTRAINT Loja_fkey FOREIGN KEY (Numero, CEP) REFERENCES Endereco (Numero, CEP)
-)
+);
 
 CREATE TABLE Cupom (
   Codigo VARCHAR2(6), -- defini que vai ter 6 codigos como cupons da shopee, ifood etc
   Desconto_porcentagem NUMBER NOT NULL,
   CONSTRAINT Cupom_pkey PRIMARY KEY (Codigo)
-)
+);
 
 CREATE TABLE Cliente_loja (
   Data_venda DATE NOT NULL, -- modelo dd/mm/yyyy
@@ -121,15 +121,15 @@ CREATE TABLE Cliente_loja (
   CONSTRAINT Cliente_loja_fkey FOREIGN KEY (ID) REFERENCES Loja(ID),
   CONSTRAINT Cliente_loja_fkey1 FOREIGN KEY (CPF) REFERENCES Cliente(CPF),
   CONSTRAINT Cliente_loja_fkey2 FOREIGN KEY (Codigo) REFERENCES Cupom(CODIGO)
-)
+);
 
 -- precisa ver isso daqui em relação a tabela de partida pq tem o problema
 CREATE TABLE Jogar (
   Equipe_Jogo VARCHAR2(5),
   Nome_Estadio VARCHAR2(255),
   Data_Jogo DATE,
-  CONSTRAINT Jogar_pkey PRIMARY KEY (Equipe_Jogo, Nome_Estadio, Data_Partida),
+  CONSTRAINT Jogar_pkey PRIMARY KEY (Equipe_Jogo, Nome_Estadio, Data_Jogo),
   CONSTRAINT Jogar_fkey1 FOREIGN KEY (Equipe_Jogo) REFERENCES Equipe(Divisao),
   CONSTRAINT Jogar_fkey2 FOREIGN KEY (Nome_Estadio) REFERENCES Estadio(Nome),
   CONSTRAINT Jogador_fkey3 FOREIGN KEY (Data_Jogo) REFERENCES Partida(Data_Partida)
-)
+);
