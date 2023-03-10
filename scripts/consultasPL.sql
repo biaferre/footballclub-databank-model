3f7e180ffc80dd9de33be5ed5c9835d367378107
claraabk <claraabk1@gmail.com>
2023-03-09 23:39:25 -0300

    checklist

--- scripts/consultasPL.sql
+++ scripts/consultasPL.sql
@@ -1,4 +1,4 @@
--- CREATE FUNCTION + CASE WHEN + SELECT INTO: Função que recebe o CPF do cliente e a data do pagamento, e retorna uma mensagem de avaliação de acordo com a loja que ele foi.
+-- CREATE FUNCTION / CASE WHEN / SELECT INTO / %TYPE
 CREATE OR REPLACE FUNCTION Avaliar (CPF Cliente_loja.CPF%TYPE, Data_Venda Cliente_loja.Data_venda%TYPE)
 RETURN VARCHAR2
 IS
@@ -25,8 +25,7 @@ END Avaliar;
 
 --SELECT Avaliar('008', TO_DATE('01/03/2023', 'dd/mm/yy')) FROM DUAL;
 
--- BLOCO ANÔNIMO:
--- Criando um bloco anônimo com um record de loja e printando o ID*/
+-- BLOCO ANÔNIMO
 DECLARE
     TYPE cupom IS RECORD (
         ID NUMBER,
@@ -38,9 +37,7 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(cupom_03.ID);
 END;
 
--- %TYPE, CASE WHEN, LOOP EXIT WHEN, CURSOR (OPEN, FETCH, CLOSE) E WHILE LOOP: 
--- Loop que analisa os jogadores cadastrados, contabilizando de acordo com a posicao
--- Cursor foi usado para pegar a posicao na tabela Jogador
+-- %TYPE / CASE WHEN / LOOP EXIT WHEN / CURSOR (OPEN, FETCH, CLOSE) / WHILE LOOP: 
 DECLARE 
     count_goleiro BINARY_INTEGER;
     count_zagueiro BINARY_INTEGER;
@@ -86,8 +83,7 @@ BEGIN
                          ', Atacante: ' || count_atacante);
 END;
 
--- CREATE PROCEDURE, ROWTYPE, CREATE OR REPLACE PACKAGE/BODY:
--- Packages com procedures para inserção de elementos do tipo Equipe e Estadio na tabela. */
+-- CREATE PROCEDURE / ROWTYPE / CREATE OR REPLACE PACKAGE/BODY:
 CREATE OR REPLACE PACKAGE cadastros AS
 PROCEDURE new_Equipe(aux Equipe%ROWTYPE);
 PROCEDURE new_Estadio(
@@ -124,6 +120,7 @@ END cadastros;
 -- SELECT * FROM Estadio;
 
 -- USO DE ESTRUTURA DE DADOS DO TIPO TABLE: Aumenta o valor do aluguel em 10% de todos os estádios, por meio de um cursor iterando sobre os dados e atualizando os valores.
+-- USO DE ESTRUTURA DE DADOS DO TIPO TABLE
 DECLARE
   TYPE EstadioTableType IS TABLE OF Estadio%ROWTYPE;
 BEGIN
@@ -154,7 +151,7 @@ END;
 
 --SELECT E3.Nome, E3.Aluguel FROM Estadio E3;
 
--- CREATE OR REPLACE PACKAGE: O código cria um pacote com uma função que retorna um cursor contendo os dados de jogos da tabela "Jogar". Essa função é chamada em um bloco anônimo que itera sobre os registros do cursor e printa os dados.
+-- CREATE OR REPLACE PACKAGE
 
 CREATE OR REPLACE PACKAGE MinhasConsultas AS
   FUNCTION ConsultarJogar RETURN SYS_REFCURSOR;
@@ -193,7 +190,6 @@ END;
 /
 
 -- RECORD, IF
--- Mostra o percentual de vitória dos jogos contra o Bayer
 DECLARE
   resultado Partida.Resultado%TYPE;
 	vitorias NUMBER;
@@ -225,8 +221,7 @@ dbms_output.put_line('Porcentagem_vitorias: ' || percentual_vitorias*100 || '%')
 
 END;
 
--- FOR IN LOOP, USO DE PARÂMETROS
--- Essa procedure fala o nome e a lotacao dos estadios da cidade recebida como entrada
+-- FOR IN LOOP / USO DE PARÂMETROS
 CREATE OR REPLACE PROCEDURE estadios_cidade(cidade_desejada IN Endereco.Cidade%TYPE)
 IS	
 	CEP Endereco.CEP%type;
@@ -253,8 +248,7 @@ END;
 EXECUTE estadios_cidade('Sao Paulo');
 
 
--- CREATE OR REPLACE TRIGGER
--- Impede que um cargo seja atualizado com salario menor que o salario mínimo.
+-- CREATE OR REPLACE TRIGGER (LINHA)
 CREATE OR REPLACE TRIGGER antes_atualizacao_cargo
 BEFORE UPDATE ON Cargos
 FOR EACH ROW
@@ -269,3 +263,104 @@ WHERE Funcao = 'Massagista';
 -- DEPOIS DE TENTAR ATUALIZAR
 SELECT C.Salario FROM Cargos C 
 WHERE C.Funcao = 'Massagista';
+-- Procedure pra mudar de reserva pra titula e vice versa
+CREATE OR REPLACE PROCEDURE mudar_escalacao (
+    cpf_jogador IN Jogador.CPF%TYPE
+)
+IS
+    antiga_escalacao Jogador.Escalacao%TYPE;
+    nova_escalacao Jogador.Escalacao%TYPE;
+    to_print VARCHAR2(255);
+BEGIN
+
+    SELECT Escalacao INTO antiga_escalacao
+    FROM Jogador WHERE CPF = cpf_jogador;
+	DBMS_OUTPUT.PUT_LINE(antiga_escalacao);
+
+    IF antiga_escalacao = 'Reserva' THEN
+        nova_escalacao := 'titular';
+        to_print := 'Parabéns! Agora esse jogador é titular';
+    ELSIF antiga_escalacao = 'Titular' THEN
+        nova_escalacao := 'reserva';
+        to_print := 'Ah não! Agora esse jogador virou um reserva, é preciso treinar mais!';
+	ELSE
+        nova_escalacao := antiga_escalacao;
+		to_print:= 'Ops! Parece que esse jogador não está escalado, vamos manter como estava!';
+    END IF;
+
+    DBMS_OUTPUT.PUT_LINE(to_print);
+	UPDATE Jogador
+        SET Escalacao = nova_escalacao
+        WHERE CPF = cpf_jogador;
+    
+END;
+
+/* BEGIN 
+    mudar_escalacao('001');
+END; */
+
+
+-- CREATE PROCEDURA
+CREATE OR REPLACE PROCEDURE cadastro_equipe (
+    nova_equipe IN Equipe%ROWTYPE
+)
+IS
+BEGIN
+    INSERT INTO Equipe (Divisao, Modalidade, Liga, CNPJ_Clube)
+    VALUES (nova_equipe.Divisao, nova_equipe.Modalidade, nova_equipe.Liga, nova_equipe.CNPJ_Clube)
+END;
+
+CREATE OR REPLACE TRIGGER data_igual_partida
+BEFORE INSERT ON Partida
+DECLARE data_existente EXCEPTION
+BEGIN
+    IF EXISTS(SELECT * FROM Partida WHERE Data_Partida = NEW.Data_Partida) THEN
+    RAISE data_existente;
+    END IF;
+EXCEPTION 
+    WHEN data_existente THEN
+    	Raise_application_error(-20003, 'Já existe uma partida com essa data!');
+END;
+
+-- EXCEPTION
+CREATE OR REPLACE TRIGGER partida_nao_existente
+BEFORE INSERT ON Partida
+FOR EACH ROW
+DECLARE 
+  partida_nao_aconteceu EXCEPTION;
+  x date;
+BEGIN
+  --SELECT Data_partida INTO x FROM Partida WHERE Data_Partida = :NEW.Data_Partida;
+  IF :NEW.Data_Partida > SYSDATE THEN
+    RAISE partida_nao_aconteceu;
+  END IF;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+  RAISE_APPLICATION_ERROR(-20002, 'Data de partida não encontrada na tabela!');
+  WHEN partida_nao_aconteceu THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Essa partida ainda não aconteceu!');
+END;
+
+/* INSERT INTO Partida(Data_Partida, Adversario, Resultado, Equipe, Estadio) 
+VALUES (TO_DATE('18/03/2023', 'dd/mm/yy'), 'Real Medeiros', 'Derrota', 'Sub20', 'Estadio Municipal');
+ */
+
+-- TRIGGER COMANDO
+CREATE OR REPLACE TRIGGER loja_fechada
+BEFORE INSERT ON Cliente_Loja
+DECLARE
+    nao_esta_aberta EXCEPTION;
+BEGIN 
+    IF EXTRACT(HOUR FROM SYSTIMESTAMP) > 19 OR EXTRACT(HOUR FROM SYSTIMESTAMP) < 8 THEN 
+        RAISE nao_esta_aberta;
+    END IF;
+EXCEPTION 
+WHEN nao_esta_aberta THEN
+    Raise_application_error(-20004,'Nossas lojas não abrem fora do horário comercial! Tente novamente amanhã :)');
+END;
+
+/* BEGIN
+    INSERT INTO Cliente_loja(Data_venda, Produto, CNPJ_Loja, CPF, ID_Cupom, Forma_pagamento, Preco)
+		VALUES (TO_DATE('01/03/2023', 'dd/mm/yy'), 'Camiseta', '0000', '001', 1, 'Cartão de débito', 49.99);
+END; */
+
