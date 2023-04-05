5f5e61c15e9f1932823a163c93aa287c22b7a639
Felipo <gabriel.felipo2010@gmail.com>
2023-04-04 23:58:32 -0300

    Entrega SGBDOR tipo, tablea e povoamento

--- SGBDOR/creationTablesOR.sql
+++ SGBDOR/creationTablesOR.sql
@@ -7,14 +7,42 @@ CREATE TABLE tb_clube OF tp_clube(
   	endereco NOT NULL
 );
 /
-    
+
+-- TABELA DE CUPOM
+CREATE TABLE tb_cupom OF tp_cupom(
+    ID PRIMARY KEY,
+    Desconto_porcentagem NOT NULL
+);
+/
+
+-- TABELA LOJA
+CREATE TABLE tb_loja OF tp_loja(
+    CNPJ PRIMARY KEY,
+    endereco NOT NULL
+);
+/
+
 -- TABELA DE CARGO
 CREATE TABLE tb_cargo OF tp_cargo(
     FUNCAO PRIMARY KEY,
     salario NOT NULL
 );
 /
-    
+
+-- TABELA DE JOGADOR
+CREATE TABLE tb_jogador OF tp_jogador(
+    CPF PRIMARY KEY,
+    data_nascimento NOT NULL,
+    nome NOT NULL,
+    sobrenome NOT NULL,
+    telefone NOT NULL,
+    numero NOT NULL,
+    posicao NOT NULL,
+    escalacao NOT NULL,
+    equipe_jogador WITH ROWID REFERENCES tp_equipe NOT NULL
+);
+/
+
 -- TABELA DE FUNCIONARIO
 CREATE TABLE tb_funcionario OF tp_funcionario(
     CPF PRIMARY KEY,
@@ -44,3 +72,16 @@ CREATE TABLE tb_cliente OF tp_cliente (
 -- Tem que rodar o povoamento antes
 SELECT c.qntd_telefones() FROM tb_cliente c WHERE p.nome = 'Felipo'
 SELECT c.nome_completo() FROM tb_cliente c WHERE p.cpf = 1
+
+-- TABELA CLIENTE_LOJA
+CREATE TABLE tb_cliente_loja OF tp_cliente_loja (
+    ID PRIMARY KEY,
+    data_venda NOT NULL,
+    produto NOT NULL,
+    forma_pagamento NOT NULL,
+    preco NOT NULL,
+    ID_cupom WITH ROWID REFERENCES tb_cupom NOT NULL,
+    CNPJ_loja WITH ROWID REFERENCES tb_loja NOT NULL,
+    CPF_cliente WITH ROWID REFERENCES tb_cliente NOT NULL
+);
+/
--- SGBDOR/creationTypesOR.sql
+++ SGBDOR/creationTypesOR.sql
@@ -8,42 +8,6 @@ CREATE OR REPLACE TYPE tp_telefone AS OBJECT (
 CREATE OR REPLACE TYPE tp_varray_telefone AS VARRAY(5) OF tp_telefone;
 /
 
--- TIPO PESSOA
-CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
-    CPF VARCHAR2 (11),
-    data_nascimento DATE,
-    nome VARCHAR2 (50),
-    sobrenome VARCHAR2 (50),
-    telefone tp_varray_telefone,
-    MEMBER FUNCTION nome_completo RETURN VARCHAR,
-    FINAL MEMBER FUNCTION qntd_telefones RETURN NUMBER
-) NOT FINAL NOT INSTANTIABLE;
-/
-
--- TIPO CLIENTE
-CREATE OR REPLACE TYPE tp_cliente UNDER tp_pessoa (
-	fidelidade NUMBER(1)
-)FINAL;
-/
-    
--- CORPO DO TIPO CLIENTE
-CREATE OR REPLACE TYPE BODY tp_cliente AS 
-    
-FINAL MEMBER FUNCTION qntd_telefones RETURN NUMBER IS
-qntTel NUMBER;
-    BEGIN
-        SELECT COUNT(*) INTO quantTel
-        FROM TABLE(SELF.telefone);
-        RETURN quantTel;
-    END;
-
-MEMBER FUNCTION nome_completo RETURN VARCHAR IS
-    BEGIN
-        RETURN  ''||nome||' '||sobrenome;
-    END;
-END;
-/
-
 -- TIPO ENDERECO
 CREATE OR REPLACE TYPE tp_endereco AS OBJECT(
     numero NUMBER(4),
@@ -53,7 +17,7 @@ CREATE OR REPLACE TYPE tp_endereco AS OBJECT(
     rua VARCHAR2(50)
 )FINAL;
 /
-    
+
 -- TIPO CLUBE
 CREATE OR REPLACE TYPE tp_clube AS OBJECT(
     CNPJ VARCHAR2(18),     
@@ -61,6 +25,145 @@ CREATE OR REPLACE TYPE tp_clube AS OBJECT(
   	Email VARCHAR2(255),      
   	endereco tp_endereco
 )FINAL;
+/
+
+-- TIPO CUPOM
+CREATE OR REPLACE TYPE tp_cupom AS OBJECT (
+  ID NUMBER,
+  Desconto_porcentagem NUMBER,
+  MEMBER FUNCTION id_cupom RETURN NUMBER,
+  MEMBER FUNCTION desconto RETURN NUMBER
+)FINAL;
+/
+
+-- TIPO LOJA
+CREATE OR REPLACE TYPE tp_loja AS OBJECT (
+    CNPJ VARCHAR2(18),
+    endereco tp_endereco
+);
+/
+
+-- TIPO PESSOA
+CREATE OR REPLACE TYPE tp_pessoa AS OBJECT(
+    CPF VARCHAR2 (11),
+    data_nascimento DATE,
+    nome VARCHAR2 (50),
+    sobrenome VARCHAR2 (50),
+    telefone tp_varray_telefone,
+    MEMBER FUNCTION nome_completo RETURN VARCHAR,
+    MEMBER PROCEDURE print_info,
+    FINAL MAP MEMBER FUNCTION qntd_telefones RETURN NUMBER
+) NOT FINAL NOT INSTANTIABLE;
+/
+
+CREATE OR REPLACE TYPE BODY tp_pessoa AS 
+
+    FINAL MAP MEMBER FUNCTION qntd_telefones RETURN NUMBER IS
+    amount NUMBER
+    BEGIN
+        SELECT COUNT(*) INTO amount 
+        FROM TABLE(SELF.telefone);
+        RETURN amount;
+    END;
+    
+    MEMBER FUNCTION nome_completo RETURN VARCHAR IS
+        BEGIN
+            RETURN  ''||nome||' '||sobrenome;
+        END;
+
+    MEMBER PROCEDURE print_info IS
+        BEGIN
+            DBMS_OUTPUT.PUT_LINE(nome);
+            DBMS_OUTPUT.PUT_LINE(cpf);
+            DBMS_OUTPUT.PUT_LINE(data_nascimento);
+        END;
+END;
+
+-- TIPO ESTÁDIO
+CREATE OR REPLACE TYPE tp_estadio AS OBJECT(
+    nome VARCHAR2(255),
+    endereco tp_endereco,
+    cnpj_clube REF tp_clube,
+    aluguel DECIMAL(8,2),
+    lotacao NUMBER(6)
+)FINAL;
+/
+
+-- TIPO JOGADOR
+CREATE OR REPLACE TYPE tp_jogador UNDER tp_pessoa(
+    numero NUMBER(2),
+    posicao VARCHAR2(255),
+    escalacao VARCHAR2(7)
+)FINAL;
+/
+
+-- NESTED JOGADOR
+CREATE OR REPLACE TYPE tp_nt_jogadores AS TABLE OF tp_jogador
+/
+
+-- TIPO EQUIPE
+CREATE OR REPLACE TYPE tp_equipe AS OBJECT(
+    divisao VARCHAR2(5),
+    modalidade VARCHAR2(255),
+    liga VARCHAR2(255),
+    cnpj_clube REF tp_clube,
+    jogadores tp_nt_jogadores
+)FINAL;
+/
+
+
+-- TIPO PARTIDA
+CREATE OR REPLACE TYPE tp_partida AS OBJECT(
+    data_partida DATE,
+    adversario VARCHAR2(255),
+    resultado VARCHAR2(255),
+    equipe REF tp_equipe,
+    estadio REF tp_estadio
+)FINAL;
+/
+
+-- TIPO JOGAR
+CREATE OR REPLACE TYPE tp_jogar AS OBJECT(
+    equipe_jogo REF tp_equipe,
+    nome_estadio REF tp_estadio,
+    data_jogo REF tp_partida
+)FINAL;
+/
+    
+-- TIPO CLIENTE
+CREATE OR REPLACE TYPE tp_cliente UNDER tp_pessoa (
+	fidelidade NUMBER(1)
+)FINAL;
+/
+    
+-- CORPO DO TIPO CLIENTE
+CREATE OR REPLACE TYPE BODY tp_cliente AS 
+	FINAL MAP MEMBER FUNCTION qntd_telefones RETURN NUMBER IS 
+    qntTel NUMBER;
+    BEGIN
+        SELECT COUNT(*) INTO quantTel
+        FROM TABLE(SELF.telefone);
+        RETURN quantTel;
+    END;
+
+	MEMBER FUNCTION nome_completo RETURN VARCHAR IS
+    BEGIN
+        RETURN  ''||nome||' '||sobrenome;
+    END;
+END;
+/
+
+-- TIPO CLIENTE-LOJA
+CREATE OR REPLACE TYPE tp_cliente_loja AS OBJECT (
+    ID NUMBER(5),
+    data_venda DATE,
+    produto VARCHAR2(255),
+    forma_pagamento VARCHAR(255),
+    preco DECIMAL(10,2),
+    ID_cupom REF tp_cupom,
+    CPF_cliente REF tp_cliente,
+    CNPJ_loja REF tp_loja
+);
 /
 
 -- TIPO CARGO
@@ -74,24 +177,28 @@ CREATE OR REPLACE TYPE tp_cargo AS OBJECT (
 CREATE OR REPLACE TYPE tp_funcionario UNDER tp_pessoa (
     supervisor REF tp_funcionario,
     CNPJ REF tp_clube,
-    cargo REF tp_cargo
+    cargo REF tp_cargo,
+    OVERRIDING MEMBER PROCEDURE print_info
 )FINAL;
 /
 
 -- CORPO DO TIPO FUNCIONARIO
 CREATE OR REPLACE TYPE BODY tp_funcionario AS 
     
-FINAL MEMBER FUNCTION qntd_telefones RETURN NUMBER IS
-qntTel NUMBER;
+	FINAL MAP MEMBER FUNCTION qntd_telefones RETURN NUMBER IS
+	qntTel NUMBER;
     BEGIN
         SELECT COUNT(*) INTO quantTel
         FROM TABLE(SELF.telefone);
         RETURN quantTel;
     END;
 
-MEMBER FUNCTION nome_completo RETURN VARCHAR IS
-    BEGIN
-        RETURN  ''||nome||' '||sobrenome;
+    OVERRIDING MEMBER PROCEDURE print_info IS
+    BEGIN 
+        DBMS_OUTPUT.PUT_LINE(nome);
+        DBMS_OUTPUT.PUT_LINE(cpf);
+        DBMS_OUTPUT.PUT_LINE(supervisor);
+        DBMS_OUTPUT.PUT_LINE(CNPJ);
     END;
 END;
 /
